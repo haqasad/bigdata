3fe433e7dfd6391beae6cf5a7baa2c70a9a917b0
Vagrant <vagrant@nowhere.com>
2015-01-26 20:20:56 +0000

    Fixed JAVA_HOME in words.sh

--- words.sh
+++ words.sh
@@ -9,13 +9,16 @@ mkdir input/alice
 cp words/alice.words.txt input/alice
 mkdir output
 rm -rf output/alice
+export JAVA_HOME=/usr/lib/jvm/default-java
 export PATH=$JAVA_HOME/bin:$PATH
 export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
-
 mkdir code
 cp bigdata/code/*.java code
+echo "Hadoop initial processing"
 hadoop/bin/hadoop com.sun.tools.javac.Main code/WordCount.java
+echo "Java jar processing"
 cd code
 jar cf wc.jar WordCount*.class
 cd ..
+echo "Hadoop execution"
 hadoop/bin/hadoop jar code/wc.jar WordCount input/alice output/alice
